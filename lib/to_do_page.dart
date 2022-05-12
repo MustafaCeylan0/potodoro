b1494613eeba56ccf957dbda980dd788e4c1ebc7
Yawnerboy <69280409+Yawnerboy@users.noreply.github.com>
2022-05-12 23:45:02 +0300

    ending Session and to-do title to home page is done.

--- lib/to_do_page.dart
+++ lib/to_do_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:potodoro/home.dart';
 
 import 'to_do_database.dart';
 
@@ -263,7 +262,6 @@ class _NoteDetailPageState extends State<NoteDetailPage> {
         appBar: AppBar(
           title: const Text("To Do List"),
           actions: [
-            goButton(),
             deleteButton(),
           ],
         ),
@@ -303,16 +301,6 @@ class _NoteDetailPageState extends State<NoteDetailPage> {
           Navigator.of(context).pop();
         },
       );
-
-  Widget goButton() => IconButton(
-    icon: const Icon(Icons.play_circle),
-    onPressed: () async {
-
-      Navigator.of(context).push(MaterialPageRoute(
-          builder: (BuildContext context) =>
-       MyHomePage(title: note.title, session: "${note.description} Session",)));
-    },
-  );
 }
 
 final _lightColors = [
@@ -418,7 +406,7 @@ class NoteFormWidget extends StatelessWidget {
         ),
         initialValue: description,
         decoration: const InputDecoration(
-          labelText: "Enter the Session Amount",
+          labelText: "Enter the description",
           icon: Icon(Icons.description),
         ),
         validator: (title) => title != null && title.isEmpty
